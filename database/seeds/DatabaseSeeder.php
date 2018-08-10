e747a2f54e33332d29cac8f04c26846032472422
pcsfilho <paulo.ecomp@gmail.com>
2018-08-09 21:18:47 -0300

    Add Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Model\Time;
+use App\Model\TimeDay;
+use App\Model\User;
+use Carbon\Carbon;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       DB::transaction(function() {
+       		//Admin
+       		$user = new User;
+	        $user->name = "Paulo Filho";
+	        $user->email = "paulo.ecomp@gmail.com";
+	        $user->registration = "11211177";
+	        $password = "123456";
+	        $user->password = bcrypt($password);
+	        $user->level = 'admin';
+	        $user->save();
+
+	        //Funcionário
+	        $user = new User;
+	        $user->name = "Lucas Assys";
+	        $user->registration = "11211178";
+	        $user->level = 'employee';
+	        $user->save();
+
+	        //Ponto do dia
+	        $time_day = new TimeDay;
+            $time_day->user_id = $user->id;
+            $time_day->date = Carbon::now()->toDayDateTimeString();
+            $time_day->save();
+
+            //Abertura e fechamento do ponto Diário
+            $time = new Time;
+            $time->time_days_id = $time_day->id;
+            $time->time_start = "8:00:00";
+            $time->time_end = "18:00:00";
+            $time->type_time = 'job';
+            $time->status = true;
+            $time->save();
+
+            //Abertura e fechamento do ponto Almoço
+            $time = new Time;
+            $time->time_days_id = $time_day->id;
+            $time->time_start = "12:00:00";
+            $time->time_end = "14:00:00";
+            $time->type_time = 'lunch';
+            $time->status = true;
+            $time->save();
+       });
     }
 }
